9a1c6b8ffe015a189e40777b0de268284e32bbe6
Air Drummer <45011117+airdrum@users.noreply.github.com>
2019-12-20 09:29:28 +0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -40,7 +40,7 @@ class DataAnalyzer:
         return fileArray
 
     def getAbsolutePath(self):
-        absFile=self.filepath +'\\'+self.name
+        absFile=self.filepath +'/'+self.name
         return absFile
 
     def readWeather(self):
@@ -147,18 +147,14 @@ class DataAnalyzer:
         return numarray, timearray
 """def pushToDatabaset(rf):
     conn = MongoClient('localhost',27017)
-
     # database
     db = conn.example
     # Created or Switched to collection names: my_gfg_collection
     collection = db.throughput_collection_fso
     #collection_fso = db.throuhgput_collection_fso
-
     for i in rf.splitlines():
         time = i.split(",")[0]
-
         throughput = i.split(",")[1]
-
         dbobject = {
                 "time": time,
                 "throughput":throughput
@@ -447,10 +443,88 @@ def removePast(old,new,timeval,rf,fso,weather):
         elif c > new:
             print(timeval[c])
         c+=1
-               
-filename="E:\\PhD"
-sys.stdout=open("E:\\PhD\\filtering_2.txt","w")
-time_str = DataAnalyzer(filename,"filtering.txt")
+
+
+def getWeekAverage(array,number):
+    if number == 31:
+        return np.average(array[0:966878])
+    elif number == 32:
+        return np.average(array[966879:2176478])
+    elif number == 33:
+        return np.average(array[2176479:3386078])
+    elif number == 34:
+        return np.average(array[3386079:4595678])
+    elif number == 35:
+        return np.average(array[4595679:5805278])
+    elif number == 36:
+        return np.average(array[5805279:7014878])
+    elif number == 37:
+        return np.average(array[7014879:8224478])
+    elif number == 38:
+        return np.average(array[8224479:9434078])
+    elif number == 39:
+        return np.average(array[9434079:10643678])
+    elif number == 40:
+        return np.average(array[10643679:11853278])
+    elif number == 41:
+        return np.average(array[11853279:13062878])
+    elif number == 42:
+        return np.average(array[13062879:14272478])
+    elif number == 43:
+        return np.average(array[14272479:-1])
+
+def getDuration(array):
+    
+    i = 1
+    switch_count=0
+    duration=0
+    switch_duration=[]
+    while i< len(array):
+        if array[i]==1 and array[i-1] == 0:
+            duration=0.5
+        elif array[i]==1 and array[i-1] == 1:
+            duration+=0.5
+        elif array[i]==0 and array[i-1] == 0:
+            if duration > 0.0:
+                switch_duration.append(duration)
+            duration=0.0
+        i+=1
+
+def getWeekSwitchCount(array,number):
+    if number == 31:
+        switch_array = array[0:966878]
+    elif number == 32:
+        switch_array = array[966879:2176478]
+    elif number == 33:
+        switch_array = array[2176479:3386078]
+    elif number == 34:
+        switch_array = array[3386079:4595678]
+    elif number == 35:
+        switch_array = array[4595679:5805278]
+    elif number == 36:
+        switch_array = array[5805279:7014878]
+    elif number == 37:
+        switch_array = array[7014879:8224478]
+    elif number == 38:
+        switch_array = array[8224479:9434078]
+    elif number == 39:
+        switch_array = array[9434079:10643678]
+    elif number == 40:
+        switch_array = array[10643679:11853278]
+    elif number == 41:
+        switch_array = array[11853279:13062878]
+    elif number == 42:
+        switch_array = array[13062879:14272478]
+    elif number == 43:
+        switch_array = array[14272479:-1]
+
+
+
+x = [1,1,1,0,0,1,0,0,0,1,0] 
+switch_dur, switch_count = getDuration(x)              
+filename="."
+#sys.stdout=open("E:\\PhD\\filtering_2.txt","w")
+time_str = DataAnalyzer(filename,"latest_dataset_rf_fso_20191219.txt")
 timeval = time_str.getFileStr().splitlines()
 ####### TAKING THE VARIABLES INTO ARRAYS#######
 timearray=[]
@@ -472,32 +546,97 @@ for i in timeval:
     except:
         fso.append(None)
 
-    weather.append(','.join(i.split(',')[3:]))
+    #weather.append(','.join(i.split(',')[3:]))
 
-###################################################
-removePast(13352900,13453700,timeval,rf,fso,weather)
-#printFsoNone(timearray,rf,fso,weather)
-sys.stdout.close()
-#print(compareThroughput(rf, fso))
-
-### STATISTICAL CHEK FSO or RF is bigger###############
-"""count_rf = 0
+count_rf = 0
 count_fso= 0
 i=0
+hard_rf_fso=[]
+switching=[]
 while i < len(rf):
     if rf[i] >= fso[i]:
         count_rf +=1
+        hard_rf_fso.append(rf[i])
+        switching.append(1)
     else:
         count_fso +=1
+        hard_rf_fso.append(fso[i])
+        switching.append(0)
     i+=1
-print("Total length: " + str(len(rf)))
-print("RF is better: " + str(count_rf))
-print("FSO is better: " + str(count_fso))
-print("RF is better by %"+ str(100.0* count_rf/len(rf)))
-print("FSO is better by %"+ str(100.0* count_rf/len(rf)))"""
-#################################################################
 
-"""plt.plot(rf)
 
-plt.plot(fso)
-plt.show()"""
+
+
+"""rf_31 = getWeekAverage(rf, 31)
+rf_32 = getWeekAverage(rf, 32)
+rf_33 = getWeekAverage(rf, 33)
+rf_34 = getWeekAverage(rf, 34)
+rf_35 = getWeekAverage(rf, 35)
+rf_36 = getWeekAverage(rf, 36)
+rf_37 = getWeekAverage(rf, 37)
+rf_38 = getWeekAverage(rf, 38)
+rf_39 = getWeekAverage(rf, 39)
+rf_40 = getWeekAverage(rf, 40)
+rf_41 = getWeekAverage(rf, 41)
+rf_42 = getWeekAverage(rf, 42)
+rf_43 = getWeekAverage(rf, 43)
+
+fso_31 = getWeekAverage(fso, 31)
+fso_32 = getWeekAverage(fso, 32)
+fso_33 = getWeekAverage(fso, 33)
+fso_34 = getWeekAverage(fso, 34)
+fso_35 = getWeekAverage(fso, 35)
+fso_36 = getWeekAverage(fso, 36)
+fso_37 = getWeekAverage(fso, 37)
+fso_38 = getWeekAverage(fso, 38)
+fso_39 = getWeekAverage(fso, 39)
+fso_40 = getWeekAverage(fso, 40)
+fso_41 = getWeekAverage(fso, 41)
+fso_42 = getWeekAverage(fso, 42)
+fso_43 = getWeekAverage(fso, 43)
+
+rffso_31 = getWeekAverage(hard_rf_fso, 31)
+rffso_32 = getWeekAverage(hard_rf_fso, 32)
+rffso_33 = getWeekAverage(hard_rf_fso, 33)
+rffso_34 = getWeekAverage(hard_rf_fso, 34)
+rffso_35 = getWeekAverage(hard_rf_fso, 35)
+rffso_36 = getWeekAverage(hard_rf_fso, 36)
+rffso_37 = getWeekAverage(hard_rf_fso, 37)
+rffso_38 = getWeekAverage(hard_rf_fso, 38)
+rffso_39 = getWeekAverage(hard_rf_fso, 39)
+rffso_40 = getWeekAverage(hard_rf_fso, 40)
+rffso_41 = getWeekAverage(hard_rf_fso, 41)
+rffso_42 = getWeekAverage(hard_rf_fso, 42)
+rffso_43 = getWeekAverage(hard_rf_fso, 43)
+
+print("31- RF: " + str(rf_31) + "Mbps , FSO: " + str(fso_31) + "Mbps, RF-FSO: " + str(rffso_31) +"Mbps")
+print("32- RF: " + str(rf_32) + "Mbps , FSO: " + str(fso_32) + "Mbps, RF-FSO: " + str(rffso_32) +"Mbps")
+print("33- RF: " + str(rf_33) + "Mbps , FSO: " + str(fso_33) + "Mbps, RF-FSO: " + str(rffso_33) +"Mbps")
+print("34- RF: " + str(rf_34) + "Mbps , FSO: " + str(fso_34) + "Mbps, RF-FSO: " + str(rffso_34) +"Mbps")
+print("35- RF: " + str(rf_35) + "Mbps , FSO: " + str(fso_35) + "Mbps, RF-FSO: " + str(rffso_35) +"Mbps")
+print("36- RF: " + str(rf_36) + "Mbps , FSO: " + str(fso_36) + "Mbps, RF-FSO: " + str(rffso_36) +"Mbps")
+print("37- RF: " + str(rf_37) + "Mbps , FSO: " + str(fso_37) + "Mbps, RF-FSO: " + str(rffso_37) +"Mbps")
+print("38- RF: " + str(rf_38) + "Mbps , FSO: " + str(fso_38) + "Mbps, RF-FSO: " + str(rffso_38) +"Mbps")
+print("39- RF: " + str(rf_39) + "Mbps , FSO: " + str(fso_39) + "Mbps, RF-FSO: " + str(rffso_39) +"Mbps")
+print("40- RF: " + str(rf_40) + "Mbps , FSO: " + str(fso_40) + "Mbps, RF-FSO: " + str(rffso_40) +"Mbps")
+print("41- RF: " + str(rf_41) + "Mbps , FSO: " + str(fso_41) + "Mbps, RF-FSO: " + str(rffso_41) +"Mbps")
+print("42- RF: " + str(rf_42) + "Mbps , FSO: " + str(fso_42) + "Mbps, RF-FSO: " + str(rffso_42) +"Mbps")
+print("43- RF: " + str(rf_43) + "Mbps , FSO: " + str(fso_43) + "Mbps, RF-FSO: " + str(rffso_43) +"Mbps")
+"""
+
+
+"""
+31- RF: 32.123Mbps , FSO: 85.861Mbps, RF-FSO: 87.206Mbps
+32- RF: 33.127Mbps , FSO: 85.713Mbps, RF-FSO: 86.602Mbps
+33- RF: 34.352Mbps , FSO: 86.576Mbps, RF-FSO: 87.830Mbps
+34- RF: 40.232Mbps , FSO: 83.781Mbps, RF-FSO: 87.140Mbps
+35- RF: 41.699Mbps , FSO: 76.555Mbps, RF-FSO: 81.003Mbps
+36- RF: 41.740Mbps , FSO: 78.845Mbps, RF-FSO: 82.418Mbps
+37- RF: 40.638Mbps , FSO: 82.372Mbps, RF-FSO: 84.690Mbps
+38- RF: 36.752Mbps , FSO: 85.669Mbps, RF-FSO: 87.956Mbps
+39- RF: 37.007Mbps , FSO: 90.152Mbps, RF-FSO: 90.962Mbps
+40- RF: 37.389Mbps , FSO: 83.195Mbps, RF-FSO: 86.241Mbps
+41- RF: 35.434Mbps , FSO: 89.823Mbps, RF-FSO: 91.193Mbps
+42- RF: 33.507Mbps , FSO: 89.699Mbps, RF-FSO: 91.133Mbps
+43- RF: 38.751Mbps , FSO: 90.152Mbps, RF-FSO: 90.634Mbps
+"""
